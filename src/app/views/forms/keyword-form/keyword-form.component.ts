ec933166a8d9f30b3d8f23f2e5c2336ca8ea67b8
root <developer.mspl@gmail.com>
2022-05-31 11:18:17 +0530

    category and subcategory in keyword

--- src/app/views/forms/keyword-form/keyword-form.component.ts
+++ src/app/views/forms/keyword-form/keyword-form.component.ts
@@ -15,7 +15,7 @@ import { map } from 'rxjs-compat/operator/map';
   styleUrls: ['./keyword-form.component.scss']
 })
 export class KeywordFormComponent implements OnInit {
-  editKeyword:FormGroup;
+  editKeyword: FormGroup;
   editData: boolean;
   submitted: boolean;
   providerResponse: any;
@@ -25,26 +25,28 @@ export class KeywordFormComponent implements OnInit {
   autoHide: number = 4000;
   horizontalPosition: MatSnackBarHorizontalPosition = 'center';
   verticalPosition: MatSnackBarVerticalPosition = 'bottom';
-  keywordForm: any ={
+  keywordForm: any = {
 
   };
   keywordFormbody = {
     keywordName: "",
     keywordType: "",
-    keywordValue : [
-     {ageGroup:{}}
+    keywordValue: [
+      { ageGroup: {} }
     ]
   }
-  daysValue:any = []
-  timeValue:any = []
-  days:any =
-  {sunday:false,
-  monday:false,
-  tuesday:false,
-  wednesday:false,
-  thursday:false,
-  friday:false,
-  saturday:false,}
+  daysValue: any = []
+  timeValue: any = []
+  days: any =
+    {
+      sunday: false,
+      monday: false,
+      tuesday: false,
+      wednesday: false,
+      thursday: false,
+      friday: false,
+      saturday: false,
+    }
   minAge: number = 3;
   maxAge: number = 10;
   ageOption: Options = {
@@ -75,121 +77,139 @@ export class KeywordFormComponent implements OnInit {
       return '' + value;
     }
   };
-  ageGroup :any = {
+  ageGroup: any = {
     from: 0,
     to: 0
   }
-  keywordValue:any=[];
+  keywordValue: any = [];
   isEmpty: any;
-  categories:any = []
-  subcategories:any =[]
+  categories: any = []
+  subcategories: any = []
+  selectedSubCategories: any = []
+  selectedCategory: any = {}
   selectedCategoryIndx = -1
   constructor(
     private apiservice: ApiService,
     public dialog: MatDialog,
     @Inject(MAT_DIALOG_DATA) public data: any,
     public dialogRef: MatDialogRef<KeywordFormComponent>,
-    private dataservice :DataService,
+    private dataservice: DataService,
     private loader: AppLoaderService,
     private snack: MatSnackBar,
     private route: Router,
   ) {
-       this.keywordForm = data;
-       this.isEmpty = Object.keys(this.keywordForm).length === 0;
-       console.log('datttttta',this.keywordForm)
+    this.keywordForm = data;
+    this.isEmpty = Object.keys(this.keywordForm).length === 0;
+    console.log('datttttta', this.keywordForm)
   }
- 
+
   getCategory() {
     this.apiservice.getCategory().subscribe(res => {
       this.loader.close();
       this.categories = res;
       console.log(res)
       // this.categories.sort((a,b) => 0 - (a.name > b.name ? -1 : 1)) 
-       });
+    });
   }
-  pushCategory(category){
-console.log(category)
+  pushCategory(e, indx) {
+    const index = this.selectedSubCategories.indexOf(this.subcategories[indx]._id)
+    if (e.checked) {
+      if (index === -1) {
+        this.selectedSubCategories.push(this.subcategories[indx]._id)
+      }
+    }
+    else {
+      if (index >= 0) {
+        this.selectedSubCategories.splice(index, 1)
+      }
+    }
+
   }
-  getTag(e,category) {
+  getTag(e, category) {
     console.log(e.checked)
-    if(e.checked){
+    if (e.checked) {
+      this.selectedCategory = category
       this.loader.open();
-        this.apiservice.getTagByCategoryId(category.id).subscribe((res: any) => {
-          this.subcategories = res.data
-          console.log(res)
-          this.subcategories = this.subcategories.filter((item) => item.isActivated === true && item.programCount);
+      this.apiservice.getTagByCategoryId(category.id).subscribe((res: any) => {
+        this.subcategories = res.data
+        this.subcategories = this.subcategories.filter((item) => item.isActivated === true && item.programCount);
         this.loader.close();
       });
     }
-    else{
+    else {
+      this.selectedSubCategories = []
+      this.selectedCategory = {}
       this.subcategories = []
     }
 
   }
   ngOnInit() {
     this.getCategory()
-    if(this.isEmpty){
-      this.editData=false;
-    }else{
-      this.editData=true;
-      if(this.keywordForm.keywordValue.length){
-      this.minAge= this.keywordForm.keywordValue[0].ageGroup.from;
-      this.maxAge= this.keywordForm.keywordValue[0].ageGroup.to;
+    if (this.isEmpty) {
+      this.editData = false;
+    } else {
+      this.editData = true;
+      if (this.keywordForm.keywordValue.length) {
+        this.minAge = this.keywordForm.keywordValue[0].ageGroup.from;
+        this.maxAge = this.keywordForm.keywordValue[0].ageGroup.to;
       }
     }
     this.editKeyword = new FormGroup({
-      keywordName: new FormControl('',Validators.required),
-      keywordType: new FormControl('',Validators.required),
+      keywordName: new FormControl('', Validators.required),
+      keywordType: new FormControl('', Validators.required),
       keywordValue: new FormControl([]),
-  });
+    });
   }
 
   addKeyword() {
-    if (this.daysValue.length==0){
-      this.days =  {
-        sunday:false,
-        monday:false,
-        tuesday:false,
-        wednesday:false,
-        thursday:false,
-        friday:false,
-        saturday:false,}    }
-    for(let i in this.daysValue){
-    let dayValue = this.daysValue[i]
-    switch (dayValue){
-      case 'sunday':
-        this.days.sunday=true;
-        break;
-        case 'monday':
-          this.days.monday=true
+    if (this.daysValue.length == 0) {
+      this.days = {
+        sunday: false,
+        monday: false,
+        tuesday: false,
+        wednesday: false,
+        thursday: false,
+        friday: false,
+        saturday: false,
+      }
+    }
+    for (let i in this.daysValue) {
+      let dayValue = this.daysValue[i]
+      switch (dayValue) {
+        case 'sunday':
+          this.days.sunday = true;
           break;
-          case 'tuesday':
-            this.days.tuesday=true
-            break;
-            case 'wednesday':
-              this.days.wednesday=true
-              break;
-              case 'thursday':
-                this.days.thursday=true
-                break;
-                case 'friday':
-                  this.days.friday=true
-                  break;
-                  case 'saturday':
-                    this.days.saturday=true
-                    break;
-    }}
+        case 'monday':
+          this.days.monday = true
+          break;
+        case 'tuesday':
+          this.days.tuesday = true
+          break;
+        case 'wednesday':
+          this.days.wednesday = true
+          break;
+        case 'thursday':
+          this.days.thursday = true
+          break;
+        case 'friday':
+          this.days.friday = true
+          break;
+        case 'saturday':
+          this.days.saturday = true
+          break;
+      }
+    }
     this.keywordFormbody.keywordName = this.editKeyword.value.keywordName;
     this.keywordFormbody.keywordType = this.editKeyword.value.keywordType;
     this.ageGroup.from = this.minAge;
     this.ageGroup.to = this.maxAge;
-    if(this.ageGroup.from || this.ageGroup.to){
+    if (this.ageGroup.from || this.ageGroup.to) {
       this.keywordFormbody.keywordValue[0].ageGroup = this.ageGroup;
     }
     console.log('addKeyword before', this.keywordFormbody)
     this.loader.open();
     this.apiservice.addKeyword(this.keywordFormbody).subscribe((res) => {
-      console.log("addKeyword res",res)
+      console.log("addKeyword res", res)
       this.providerResponse = res;
       this.loader.close();
       if (this.providerResponse.isSuccess === true) {
@@ -197,20 +217,20 @@ console.log(category)
         this.snack.open(this.message, 'OK', { duration: 7000 });
         this.route.navigate(['tables/keyword']);
       } else {
-        
-          let msg = 'Somthing went wrong!';
-          this.snack.open(msg, 'OK', { duration: 7000 });
+
+        let msg = 'Somthing went wrong!';
+        this.snack.open(msg, 'OK', { duration: 7000 });
       }
     });
   }
 
   updateKeyword() {
-      this.keywordForm.keywordValue[0].ageGroup.from = this.minAge;
-      this.keywordForm.keywordValue[0].ageGroup.to = this.maxAge;
-      console.log('updateKeyword before',this.keywordForm)
+    this.keywordForm.keywordValue[0].ageGroup.from = this.minAge;
+    this.keywordForm.keywordValue[0].ageGroup.to = this.maxAge;
+    console.log('updateKeyword before', this.keywordForm)
     this.loader.open();
-    this.apiservice.updateKeyword(this.keywordForm._id,this.keywordForm).subscribe((res) => {
-      console.log("updateKeyword res",res)
+    this.apiservice.updateKeyword(this.keywordForm._id, this.keywordForm).subscribe((res) => {
+      console.log("updateKeyword res", res)
       this.providerResponse = res;
       this.loader.close();
       if (this.providerResponse.isSuccess === true) {
@@ -218,9 +238,9 @@ console.log(category)
         this.snack.open('Keyword updated successfully', 'OK', { duration: 7000 });
         this.route.navigate(['tables/keyword']);
       } else {
-        
-          let msg = 'Somthing went wrong!';
-          this.snack.open(msg, 'OK', { duration: 7000 });
+
+        let msg = 'Somthing went wrong!';
+        this.snack.open(msg, 'OK', { duration: 7000 });
       }
     });
   }
@@ -229,10 +249,10 @@ console.log(category)
 
   onSubmit() {
     this.submitted = true;
-    if(this.editData){
-     this.updateKeyword();
-    }else{
-     this.addKeyword();
+    if (this.editData) {
+      this.updateKeyword();
+    } else {
+      this.addKeyword();
     }
   }
 }
